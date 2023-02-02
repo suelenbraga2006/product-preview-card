1832e34a92967ac9d0e8d913ec70454475a23d86
Suelen Braga <suelen.braga2006@gmail.com>
2023-02-02 15:22:28 -0300

    Criação do componente Card

--- src/App.js
+++ src/App.js
@@ -1,34 +1,8 @@
 import './App.scss';
-
-import { BsCart3 } from 'react-icons/bs';
-import bitmap from './assets/images/bitmap.png';
+import Card from './components/Card';
 
 function App() {
-  return (
-    <div className="container">
-      <div className="image">
-        <img src={bitmap} alt="Gabrielle Essence Eau De Parfum" />
-      </div>
-      <div className="info">
-        <small>PERFUME</small>
-        <h1>Gabrielle Essence Eau De Parfum</h1>
-        <p>
-          A floral, solar and voluptuous interpretation composed by Olivier
-          Polge, Perfumer-Creator for the House of CHANEL.
-        </p>
-        <div className="price">
-          <p>
-            $149.99
-            <span>$169.99</span>
-          </p>
-        </div>
-        <a href="/home">
-          <BsCart3 size={15} />
-          Add to Cart
-        </a>
-      </div>
-    </div>
-  );
+  return <Card />;
 }
 
 export default App;
