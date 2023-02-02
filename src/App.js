fc899a52d6dc4444f7503b74be5159b9b5b8cc85
Suelen Braga <suelen.braga2006@gmail.com>
2023-02-02 13:43:41 -0300

    Tudo pronto

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+
+import { BsCart3 } from 'react-icons/bs';
+import bitmap from './assets/images/bitmap.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <div className="image">
+        <img src={bitmap} alt="Gabrielle Essence Eau De Parfum" />
+      </div>
+      <div className="info">
+        <small>PERFUME</small>
+        <h1>Gabrielle Essence Eau De Parfum</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          A floral, solar and voluptuous interpretation composed by Olivier
+          Polge, Perfumer-Creator for the House of CHANEL.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <div className="price">
+          <p>
+            $149.99
+            <span>$169.99</span>
+          </p>
+        </div>
+        <a href="/home">
+          <BsCart3 size={15} />
+          Add to Cart
         </a>
-      </header>
+      </div>
     </div>
   );
 }
